11b53f5e08126f7655cf9e471a4654282ce03227
adnan683515 <golamfaruk680@gmail.com>
2025-05-24 21:14:00 +0600

    update korlam

--- src/Components/BrowseListing.jsx
+++ src/Components/BrowseListing.jsx
@@ -32,13 +32,15 @@ const BrowseListing = () => {
             <div className="container p-2 mx-auto sm:p-4 ">
                 <div className='sm:flex mt-4 sm:mt-0 justify-between'>
                     <h2 className="mb-4 sm:text-2xl text-xl sm:text-start text-center font-semibold leading-tight">All Post showing</h2>
-                    <div className='flex flex-col sm:flex-row  gap-2 my-2 sm:my-0'>
+                    <div className='hidden sm:block'>
+                        <div className='flex  gap-2 my-2 sm:my-0'>
                         <div className='flex justify-center items-center'>
                             <input onChange={(e) => setSearchValue(e.target.value)} className='px-2 py-1 rounded-tl-xl rounded-br-xl border  border-teal-500' placeholder='search by location' type="text" name="search" id="" />
                         </div>
                         <div className='flex justify-center items-center'>
                             <button onClick={handlesearch} className='px-2 rounded-bl-xl rounded-tr-xl py-1 bg-teal-600 text-white'>search</button>
                         </div>
+                    </div>
                     </div>
                 </div>
                 <div className="overflow-x-auto">
