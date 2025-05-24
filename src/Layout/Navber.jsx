b0e5db57ccd4830b613ffb895ae85f99e497554c
adnan683515 <golamfaruk680@gmail.com>
2025-05-24 23:16:29 +0600

    update navber

--- src/Layout/Navber.jsx
+++ src/Layout/Navber.jsx
@@ -38,8 +38,8 @@ const Navber = () => {
 
     const links = <>
 
-        <NavLink className={'block md:hidden'} to={'/'} >Home</NavLink>
-        <NavLink to={'/addtoroommate'} >Add to Find Roommate </NavLink>
+        <NavLink  to={'/'} >Home</NavLink>
+        <NavLink  to={'/addtoroommate'} >Add to Find Roommate </NavLink>
         <NavLink to={'/browseListing'} >Browse Listing</NavLink>
         <NavLink to={`/mylisting/${user?.email}`} >My Listings</NavLink>
 
